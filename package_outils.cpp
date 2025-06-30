7cec507c080a6e021f9999a73454e47fa302aaeb
kelian8 <165786423+kelian8@users.noreply.github.com>
2025-06-30 16:41:01 +0200

    Update package_outils.cpp

--- package_outils.cpp
+++ package_outils.cpp
@@ -4,9 +4,9 @@
 #include <iostream>
 #include <unordered_set>
 
-
+//fonction permettant de passer d'une table de bordures à  une table de préfixes
 std::vector<int> BorderToPlist(std::vector<int> b){
-  std::vector<int> L; //liste vide pour renvoyer qui sera resultat
+  std::vector<int> L; //liste vide à renvoyer qui servira de  resultat
   int l; //permet de stocker la valeur de b a l'indice i
   int i = b.size()-1; // indice permettant de parcourir la liste de la droite vers la gauche 
   
@@ -29,9 +29,10 @@ std::vector<int> BorderToPlist(std::vector<int> b){
 
 //////////////////
 
+// fonction permettant de passer d'une table de préfixes à une liste de préfixes
 std::vector<int> PrefixToList(const std::vector<int>& P) {
-  std::vector<int> L;  //creation d'une liste vide pour les resultats
-  int i = P.size() - 1; //indice permettant de parcourir la liste de droite a gauche 
+  std::vector<int> L;  //création d'une liste vide pour les résultats
+  int i = P.size() - 1; //indice permettant de parcourir la liste de droite à gauche 
 
     while (i > 0) {
       std::vector<int> I; //liste pour stocker les indices de 1 à i respectant la condition
@@ -42,16 +43,16 @@ std::vector<int> PrefixToList(const std::vector<int>& P) {
         }
 
         int l;
-        if (I.empty()) { // si la liste est vide l = 0 et on decremente i de 1
+        if (I.empty()) { // si la liste est vide l = 0 et on décremente i de 1
             l = 0;
             i = i - 1;
-        } else {        //si elle n'est pas vide l = i - minI + 1 et on decremente i de minI-1
+        } else {        //si elle n'est pas vide l = i - minI + 1 et on décremente i de minI-1
             int minI = *std::min_element(I.begin(), I.end());
             l = i - minI + 1;
             i = minI - 1;
         }
         
-        L.insert(L.begin(), l);  // on ajoute l au debut de L
+        L.insert(L.begin(), l);  // on ajoute l au début de L
     }
 
     return L;
@@ -61,9 +62,8 @@ std::vector<int> PrefixToList(const std::vector<int>& P) {
 
 //////////////////
 
-
-
-std::vector<std::string> creaAlph(int n){//construit un alphabet [µ1,µ2,...µn-1]
+//fonction permettant de construire  un alphabet [µ1,µ2,...µn-1]
+std::vector<std::string> creaAlph(int n){
   std::vector<std::string> alphabet;
   for (int i =1;i<n;i++){
     alphabet.push_back("µ"+ std::to_string(i));   
@@ -72,21 +72,10 @@ std::vector<std::string> creaAlph(int n){//construit un alphabet [µ1,µ2,...µn
   return alphabet;
 }
 
-/*  
-std::string nobord(std::vector<std::string> w, std::vector<std::string> alphabet){
-  for(int i =0;i<alphabet.size();i++){
-    
-    if (std::find(w.begin(), w.end(), alphabet[i]) == w.end()){
-      //std::find(V.itera1,V.itera2,value) renvoit  V.ietra2 si la valeur value nest pas dans V et sinon l'indice de la 1 ere occurence de la valeur
-      
-	return alphabet[i];
-	  }
-  }
-  return "alphabet trop petit";
-}
-*/
 
+///////////////////
 
+//fonction permettant de renvoyer la première lettre la première à ajouter au mot, contenue dans l'aphabet permettant de créer un 0 dans la table de bords 
 std::string nobord(std::vector<std::string> w, std::vector<std::string> alphabet) {
     if (w.empty()) {
         return alphabet.size() > 0 ? alphabet[0] : "alphabet trop petit";
@@ -125,7 +114,7 @@ std::string nobord(std::vector<std::string> w, std::vector<std::string> alphabet
 
 ////////////////
 
-
+//fonction permettant de passer d'une liste de préfixe à un mot crée sur un alphabet
 std::string LPtoWord(std::vector<int> lp, std::vector<std::string> alpha){
   std::vector<std::string> w(100,"-1"); //créer un tableau vide de 100 case remplies de -1 pour stocker les lettres du mots dans l'ordre 
   w[0] = alpha[0]; //la première lettre du mot est la prmière lettre de l'alphabet 
@@ -157,6 +146,7 @@ std::string LPtoWord(std::vector<int> lp, std::vector<std::string> alpha){
 
 ////////////////////////////////////
 
+//fonction permettant de passer d'un mot à une table de bordures
 std::vector<int> WtoB(std::string w){
   std::vector<int> res(w.size(),0);//initialistaion du tableau de bordures de taille=longueur de w avec des 0
   
@@ -179,7 +169,8 @@ std::vector<int> WtoB(std::string w){
 
 ///////////////////////////
 
-
+//fonction permettant de calculer la longueur du plus long bord du mot ua, où u est un mot,
+// B est la table de bordures de u, et a est une lettre à ajouter.
 int Border(std::vector<int> b, std::vector<std::string> u, std::string a){
   if(u.size()>0){
     int i =b[(u.size()-1)];
@@ -197,6 +188,8 @@ int Border(std::vector<int> b, std::vector<std::string> u, std::string a){
     return 1;
   }
 }
+
+//fonction permettant de passer d'une liste de préfixes à un mot minimal (càd avec le moins de lettres différentes possible) sur un alphabet
 std::string LtoMinW(std::vector<int> l, std::vector<std::string> alpha){
   std::vector<int> k(100,0);
   k[0]=0;
@@ -236,91 +229,8 @@ std::string LtoMinW(std::vector<int> l, std::vector<std::string> alpha){
 
 
 //////////////////
-/*
-void generate_string_combinations_helper(const std::string& str,const std::vector<std::string>& symbols,std::vector<std::string>& result,std::string current,size_t index,const std::vector<size_t> zero_indices) {
-    if (index == zero_indices.size()) {
-        result.push_back(current);
-        return;
-    }
 
-    size_t zero_pos = zero_indices[index];
-    for (const auto& symbol : symbols) {
-        // Remplacer '0' par le symbole entier
-        std::string temp = current;
-        temp.replace(zero_pos, 1, symbol);
-        generate_string_combinations_helper(str, symbols, result, temp, index + 1, zero_indices);
-    }
-}
-
-std::vector<std::string> generate_string_combinations(const std::string& str,const std::vector<std::string>& symbols) {
-  if (symbols.empty()) {
-        for (char c : str) {
-            if (c == '0') {
-                return {};
-            }
-        }
-    }
-
-    std::vector<size_t> zero_indices;
-    for (size_t i = 0; i < str.size(); ++i) {
-        if (str[i] == '0') {
-            zero_indices.push_back(i);
-        }
-    }
-
-    std::vector<std::string> result;
-    std::string current = str;
-
-    if (zero_indices.empty()) {
-        result.push_back(str);
-        return result;
-    }
-
-    generate_string_combinations_helper(str, symbols, result, current, 0, zero_indices);
-    return result;
-} 
-
-
-std::vector<std::string> TBtoPW(std::vector<int> tb, std::vector<std::string> alpha ){
-  std::vector<std::string> res;
-  std::string mot = LPtoWord(BorderToPlist(tb),alpha);
-  std::vector<int> pos;
-  for(int i =1; i<tb.size();i++){
-    if (tb[i]==0){
-      pos.push_back(i);
-    }
-  }
-  for(int i=0;i<pos.size();i++){
-    mot[pos[i]]='0';
-  }
-  res = generate_string_combinations(mot, alpha);
-  for(int i =0;i<res.size();i++){
-    if( WtoB(res[i])!=tb){
-      res.erase(res.begin() + i);
-      i--;
-    }
-    int maxj=0;
-    for(int j=0;j<res[i].size();j++){
-
-      std::string symbol(1, res[i][j]);
-      auto itera = std::find(alpha.begin(), alpha.end(),symbol );
-      int indice = std::distance(alpha.begin(), itera);
-      if(indice>maxj+1){
-	res.erase(res.begin()+i);
-	i--;
-	break;
-      }
-      else{
-	if(maxj<indice){
-	  maxj++;
-	}
-      }
-    }
-  }
-  return res;
-}
-*/
-
+//fonction permttant de renvoyer les lettres interdites à placer à l'indice i d'un mot, pour pouvoir respecter le 0 à l'indice i de la table de bords
 std::vector<std::string> bannedletter(std::vector<int> tb, std::string w,int i){//algo qui etant donné un tableau de bordures et un mot ainsi qu'un indice, renvoit un tableau  de lettre interdite à placer à l'indice i afin de respecter le tableau de bordures.
   std::vector<std::string> res;//création du tableau à renvoyer
   res.push_back(std::string(1,w[0]));// 1 ère lettre interdite : la première lettre du mot pour pas avoir une bordure à 1
@@ -328,38 +238,6 @@ std::vector<std::string> bannedletter(std::vector<int> tb, std::string w,int i){
   return res;
 }
 
-/*
-std::vector<std::string>  BtoPW(std::vector<int> tb , std::vector<std::string> alpha){
-  std::vector<std::string> res;
-  int maxlet =0;
-  std::vector<int> pos;
-  std::vector<std::string> banl;
-  std::string w;
-  w+=alpha[0];
-  
-
-  for(int i=1;i<tb.size();i++){
-    
-    if(tb[i]==0){
-      banl=bannedletter(tb, w,i);
-      
-      for(int j=0; j<maxlet+2;j++){
-	if(std::find(banl.begin(), banl.end(), alpha[j]) == banl.end()){
-	  w+=alpha[j];
-	}
-      }
-    }
-    else{
-      w+=w[tb[i]-1];
-    }
-  }
-  res.push_back(w);
-
-
-  return res;
-}
-  
-*/
 
 // Fonction récursive pour générer tous les mots p-canoniques
 void generate_words(std::vector<int> tb, std::vector<std::string> alpha, std::string w, int i, int maxlet, std::vector<std::string>& res) {
