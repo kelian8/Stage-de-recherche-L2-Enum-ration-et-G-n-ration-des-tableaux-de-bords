d3c78552ee3599f66477660e4a0499a14ccd06d5
kelian8 <165786423+kelian8@users.noreply.github.com>
2025-06-30 17:53:04 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,16 +8,20 @@
 
 int main(){
   printf( "ca commence \n");
-  
-  std::vector<int> tb = {0,1,0,1,2};
 
-  std::vector<int> tp = {5,0,0,1,0};
+  printf("***********************************************************\n");
+  
+  std::vector<int> tb = {0,0,0,1,2,0,0};// choisir sa table de bordures à tester
+
+  std::vector<int> tp = {7,0,0,2,0,0,0};// choisir sa table de préfixes à tester
   
   std::vector<int> lp = BorderToPlist(tb);
 
    std::vector<int> lp2 = PrefixToList(tp);
 
-  printf("Avec BorderToLPlist : [");
+   
+  // affichage de BorderToPlist et de PrefixToList
+  printf("Avec BorderToPlist : [");
     
   for (int i=0; i<lp.size();i++){
     std::cout << lp[i];
@@ -41,11 +45,13 @@ int main(){
   printf("] \n");
 
 
-  std::vector<std::string> alphabet = creaAlph(26);
+  std::vector<std::string> alphabet = creaAlph(26);// créatuion d'un alphabet de 26 caractères
 
   
   
   std::string w = LPtoWord(lp, alphabet);
+
+  //affichage de LPtoWord
   printf("voici le mot : ");
   std::cout << w ;
 
@@ -54,7 +60,7 @@ int main(){
 
   printf("***********************************************************\n");
   
-  
+  //affichage de nobord
   std::vector<std::string> wtest = {"µ1","µ1","µ2","µ1","µ3"};
   printf("test nobord([µ1,µ1,µ2,µ1,µ3], alphabet) : ");
   std::cout<<nobord(wtest, alphabet);
@@ -63,8 +69,9 @@ int main(){
 
   printf("***********************************************************\n");
 
-  std::string w2 = "ababbba";
-  
+  std::string w2 = "ababbba"; //choisir son mot
+
+  //affichage des tests de WtoB
   printf("test WtoB avec ");
   std::cout<<w2;
 
@@ -85,8 +92,9 @@ int main(){
 
   printf("***********************************************************\n");
 
+  //affichage de LtoMinW
   printf("test de LtoMinW avec liste de préfixes = ");
-  std::vector<int> tb2 = {0,0,1,2,0};
+  std::vector<int> tb2 = {0,0,1,2,0}; //choisir son tableau de bords
   std::vector<int> lp3 = BorderToPlist(tbw);
 
   printf("[");
@@ -106,46 +114,13 @@ int main(){
   printf("\n");
   
   printf("***********************************************************\n");
-  /*
-  std::vector<int> tb3 = {0,0,1,2,3};
-  std::vector<std::string> alpha2= {"a","b","c","d","e","f"};
-  std::vector<std::string> listemotsp = TBtoPW(tb3,alpha2);
-  
-  printf("voici la liste  de taille  ");
-  std::cout<<listemotsp.size();
-  printf(", correspondant a la table de bordure : [");
-  for (int i=0; i<tb3.size();i++){
-    std::cout << tb3[i];
-    if (i!=(tb3.size()-1)){
-      printf(",");
-    }
-  }
-  printf("]");
-  printf(" sur l'alphabet : [");
-  for (int i=0; i<alpha2.size();i++){
-    std::cout << alpha2[i];
-    if (i!=(alpha2.size()-1)){
-      printf(",");
-    }
-  }
-  printf("]");
-  printf(": \n");
-  printf("[");
-  for (int i=0; i<listemotsp.size();i++){
-    std::cout << listemotsp[i];
-    if (i!=(listemotsp.size()-1)){
-      printf(",");
-    }
-  }
-  printf("]\n");
 
-  printf("***********************************************************\n");
-  */
 
-  std::vector<int> tb3 = {0,0,0,1,2,0,0};
-  std::vector<std::string> alpha2= {"a","b","c","d"};
+  std::vector<int> tb3 = {0,0,0,1,2,0,0};// choisir son tableau de bords
+  std::vector<std::string> alpha2= {"a","b","c","d"};//choisir son alphabet
   std::vector<std::string> listemotsp = BtoPW(tb3,alpha2);
 
+  //affichage de listemotsp
   printf("voici la liste  de taille  ");
   std::cout<<listemotsp.size();
   printf(", correspondant a la table de bordure : [");
